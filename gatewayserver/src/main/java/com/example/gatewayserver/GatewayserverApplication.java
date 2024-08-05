941d052df1f758364f5eb70c48c8f618c587b923
yyalcin <yusuf.yalcin@orioninc.com>
2024-08-05 17:59:50 +0300

    Gateway server in progress

---
+++ gatewayserver/src/main/java/com/example/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,33 @@
+package com.example.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator furkanBankRouteConfig(RouteLocatorBuilder routeLocatorBuilder) {
+		return routeLocatorBuilder.routes()
+				.route(predicateSpec ->
+						predicateSpec.path("/furkanbank/accounts/**")
+						.filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath("/furkanbank/accounts/(?<segment>.*)","/${segment}"))
+								.uri("lb://ACCOUNTS"))
+				.route(predicateSpec ->
+						predicateSpec.path("/furkanbank/cards/**")
+								.filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath("/furkanbank/cards/(?<segment>.*)","/${segment}"))
+								.uri("lb://CARDS"))
+				.route(predicateSpec ->
+						predicateSpec.path("/furkanbank/loans/**")
+								.filters(gatewayFilterSpec -> gatewayFilterSpec.rewritePath("/furkanbank/loans/(?<segment>.*)","/${segment}"))
+								.uri("lb://LOANS")).build();
+	}
+
+}
