1b7fb9226f96b9d120887e5a0edc3b89945aa0f0
yyalcin <yusuf.yalcin@orioninc.com>
2024-08-16 16:50:49 +0300

    Bugs

--- loans/src/main/java/com/example/loans/LoansApplication.java
+++ loans/src/main/java/com/example/loans/LoansApplication.java
@@ -1,12 +1,15 @@
 package com.example.loans;
 
+import com.example.loans.dto.LoansContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
 @EnableFeignClients
 public class LoansApplication {
 
--- loans/src/main/java/com/example/loans/dto/LoansContactInfoDto.java
+++ loans/src/main/java/com/example/loans/dto/LoansContactInfoDto.java
@@ -1,5 +1,6 @@
 package com.example.loans.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
@@ -8,7 +9,9 @@ import java.util.Map;
 
 @ConfigurationProperties(prefix = "loans")
 @Data
+@AllArgsConstructor
 public class LoansContactInfoDto {
+
     private String message;
     private Map<String, String> contactDetails;
     private List<String> onCallSupport;
