ceecd6e14b93454dd474afc6259819d66febc300
Yusuf Furkan Yalçın <yusuffurkanyalcinn@gmail.com>
2024-09-23 23:52:58 +0300

    Comments

--- gatewayserver/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
+++ gatewayserver/src/main/java/com/example/gatewayserver/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -41,4 +40,3 @@ public class SecurityConfig {
         return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
-
